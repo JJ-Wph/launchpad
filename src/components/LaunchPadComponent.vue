74d7b5f877f9ebed99ca1a09ae71185d9fabe7fb
JJ-Wph <kuba.wypych92@gmail.com>
2022-08-02 20:01:22 +0200

    new features

--- src/components/LaunchPadComponent.vue
+++ src/components/LaunchPadComponent.vue
@@ -1,31 +1,37 @@
 <template>
-  <div class="main">
-    <button @click="playAudio1">1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>4</button>
-    <button>5</button>
-    <button>6</button>
-    <button>7</button>
-    <button>8</button>
-    <button>9</button>
-    <button>10</button>
-    <button>11</button>
-    <button>12</button>
+  <div class="main" @keyup.q="playSound('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')">
+    <button @click="playSound('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')" 
+            @keyup.q="playSound('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')">Q</button>
+
+
+
+
+    <button @click="playSound('https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/EMU%20SP%2012%20Kult/34[kb]sp12-28.wav.mp3')" 
+            @keyup.w="playSound">W</button>
+    <button @click="playSound('https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/EMU%20SP%2012%20Kult/25[kb]sp12-26.wav.mp3')" @keyup.e="playSound">E</button>
+    <button @click.prevent="playSound('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')" @keyup.q="playSound">A</button>
+    <button @click.prevent="playSound('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')" @keyup.q="playSound">S</button>
+    <button @click.prevent="playSound('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')" @keyup.q="playSound">D</button>
+    <button @click.prevent="playSound('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')" @keyup.q="playSound">Z</button>
+    <button @click.prevent="playSound('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')" @keyup.q="playSound">X</button>
+    <button @click.prevent="playSound('')" @keyup.q="playSound">C</button>
   </div>
 </template>
 
 <script>
-    const audio1 = new Audio("launchpad/src/assets/mouse-click.mp3");
 
 export default {
-    data: () => ({
-         
-    }),
-
+    data() {
+        return {
+            
+        }
+    },
     methods: {
-        playAudio1(){
-            audio1.play()
+        playSound(sound) {
+            if(sound) {
+                var audio = new Audio(sound);
+                audio.play();
+            }
         }
     }
 }
@@ -40,8 +46,10 @@ div{
     justify-content: center;
     flex-direction: row;
     width: 50%;
+    padding: 40px;
     margin-top: 4vh;
     margin-bottom: 2vh;
+    background-color: cyan;
 }
 
 button{
