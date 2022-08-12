9996c73df3af050e398b93b077c353f0c6f950b3
JJ-Wph <kuba.wypych92@gmail.com>
2022-08-12 13:52:45 +0200

    ewasfrd

--- src/components/LaunchPadComponent.vue
+++ src/components/LaunchPadComponent.vue
@@ -133,7 +133,7 @@
             powerOnOrOff() {
                 if(this.isOn==false) {
                         this.qSound.link = '';
-                        this.wSound.link = './audio/BassF24.wav';
+                        this.wSound.link = './components/audio/BassF24.wav';
                         this.aSound.link = '';
                         this.sSound.link = '';
                         this.dSound.link = '';
